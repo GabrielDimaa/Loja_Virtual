31a781fdbc023acbb1cd8358ac870e9e1b0fc4a8
GabrielDimaa <gabrielmatos1999@rede.ulbra.br>
2020-11-14 18:07:37 -0300

    -

---
+++ lib/screens/product_screen.dart
@@ -0,0 +1,158 @@
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:flutter/material.dart';
+import 'package:loja_virtual/data/cart_produtc.dart';
+import 'package:loja_virtual/data/product_data.dart';
+import 'package:loja_virtual/models/cart_model.dart';
+import 'package:loja_virtual/models/user_model.dart';
+import 'package:loja_virtual/screens/cart_screen.dart';
+import 'package:loja_virtual/screens/login_screen.dart';
+
+class ProductScreen extends StatefulWidget {
+	final ProductData product;
+
+	ProductScreen(this.product);
+
+    @override
+    _ProductScreenState createState() => _ProductScreenState(product);
+}
+
+class _ProductScreenState extends State<ProductScreen> {
+	final ProductData product;
+
+	String sizeProduct;
+
+	_ProductScreenState(this.product);
+
+	@override
+	Widget build(BuildContext context) {
+		final Color primaryColor = Theme.of(context).primaryColor;
+
+		return Scaffold(
+			appBar: AppBar(
+				title: Text(product.title),
+				centerTitle: true,
+			),
+			body: ListView(
+				children: <Widget>[
+					AspectRatio(
+						aspectRatio: 0.9,
+						child: Carousel(
+							images: product.images.map((url) {
+								return NetworkImage(url);
+							}).toList(),
+							dotSize: 4.0,
+							dotSpacing: 15.0,
+							dotBgColor: Colors.transparent,
+							dotColor: primaryColor,
+							autoplay: false
+						)
+					),
+					Padding(
+						padding: EdgeInsets.all(16.0),
+						child: Column(
+							crossAxisAlignment: CrossAxisAlignment.stretch,
+							children: <Widget>[
+								Text(product.title,
+									style: TextStyle(
+										fontSize: 20.0,
+										fontWeight: FontWeight.w500
+									),
+									maxLines: 3
+								),
+								Text("R\$ ${product.price.toStringAsFixed(2)}",
+									style: TextStyle(
+										fontSize: 22.0,
+										fontWeight: FontWeight.bold,
+										color: primaryColor
+									)
+								),
+								SizedBox(height: 16.0),
+								Text("Tamanho",
+									style: TextStyle(
+										fontSize: 16.0,
+										fontWeight: FontWeight.w500
+									)
+								),
+								SizedBox(
+									height: 34.0,
+									child: GridView(
+										scrollDirection: Axis.horizontal,
+										padding: EdgeInsets.symmetric(vertical: 4.0),
+										gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+											crossAxisCount: 1,
+											mainAxisSpacing: 8.0,
+											childAspectRatio: 0.5
+										),
+										children: product.sizes.map((size) {
+											return GestureDetector(
+												onTap: () {
+													setState(() {
+														sizeProduct = size;
+													});
+												},
+												child: Container(
+													decoration: BoxDecoration(
+														borderRadius: BorderRadius.all(Radius.circular(4.0)),
+														border: Border.all(
+															color: sizeProduct == size ? primaryColor : Colors.grey[500],
+															width: 2.0
+														)
+													),
+													width: 50.0,
+													alignment: Alignment.center,
+													child: Text(size)
+												)
+											);
+										}).toList()
+									)
+								),
+								SizedBox(height: 16.0),
+								SizedBox(
+									height: 44.0,
+									child: RaisedButton(
+										onPressed: sizeProduct != null ? () {
+											if(UserModel.of(context).isLoggedIn()) {
+												CartProduct cartProduct = CartProduct();
+												cartProduct.size = sizeProduct;
+												cartProduct.quantity = 1;
+												cartProduct.productId = product.id;
+												cartProduct.category = product.category;
+
+												CartModel.of(context).addCartItem(cartProduct);
+
+												Navigator.of(context).push(
+													MaterialPageRoute(builder: (context) => CartScreen())
+												);
+											} else {
+												Navigator.of(context).push(
+													MaterialPageRoute(builder: (context) => LoginScreen())
+												);
+											}
+										} : null,
+										child: Text("Adicionar ao Carrinho",
+											style: TextStyle(fontSize: 18.0)
+										),
+										color: primaryColor,
+										textColor: Colors.white
+									)
+								),
+								SizedBox(height: 16.0),
+								Text("Descrição",
+									style: TextStyle(
+										fontSize: 16.0,
+										fontWeight: FontWeight.w500
+									)
+								),
+								Text(product.description,
+									style: TextStyle(
+										fontSize: 16.0
+									)
+								)
+							]
+						)
+					)
+				]
+			)
+		);
+	}
+}
